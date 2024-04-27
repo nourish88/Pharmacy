6250403fa164867d4160080c34ab91f844f298eb
nuriaktas <aktasnuri88@gmail.com>
2024-04-27 15:22:37 +0300

    asd

---
+++ Migrations/20240427073441_InitialCreates.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pharmacy.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SurName",
+                table: "Customers",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SurName",
+                table: "Customers");
+        }
+    }
+}
